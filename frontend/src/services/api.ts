f505fb7ac9c5e4c81f817a4a10d98409ef63cd4d
Yashu <yashaswinir483@gmail.com>
2026-03-19 16:31:46 +0530

    fixed backend url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://finance-backend-mkpr.onrender.com",
+  baseURL: "https://finance-backend-mkpr.onrender.com"
 });
 
 // Attach token automatically
